1e5c03df3ddcfe3f61cc6d765b0271cffbfdf2aa
jp9000 <obs.jim@gmail.com>
2014-11-19 04:57:13 -0800

    win-capture: Add cursor capture capability

---
+++ plugins/win-capture/cursor-capture.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdint.h>
+
+struct cursor_data {
+	gs_texture_t                   *texture;
+	HCURSOR                        current_cursor;
+	POINT                          cursor_pos;
+	long                           x_hotspot;
+	long                           y_hotspot;
+	bool                           visible;
+};
+
+extern void cursor_capture(struct cursor_data *data);
+extern void cursor_draw(struct cursor_data *data, long x_offset, long y_offset);
+extern void cursor_data_free(struct cursor_data *data);
